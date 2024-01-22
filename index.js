cf8b9f60a2e5c52630808b018b6a99da03ee95fa
Marvin <okongomarvin971@gmail.com>
2024-01-22 22:33:41 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+
+    const configurationObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        }),
+    };
+
+    // Return the fetch() so that the tests can access it
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            return data;
+        })
+        .catch(function (error) {
+            alert("Unauthorized Access");
+            console.log(error.message);
+        });
+}
